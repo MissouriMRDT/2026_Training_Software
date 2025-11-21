ac3b12972500d386835494092d7064f60ad86de8
AronKat123 <aron.chip.katon@gmail.com>
2025-11-20 20:27:44 -0600

    Add Flashing LED

--- src/TrainingSoftware.cpp
+++ src/TrainingSoftware.cpp
@@ -1,9 +1,26 @@
+#include "TrainingSoftware.h"
 #include <Arduino.h>
+#include <servo.h>
 
 void setup() {
-    // put your setup code here, to run once:
+    pinMode(33, OUTPUT);
+    digitalWrite(33, HIGH); 
+
+    pinMode(10, OUTPUT);
+    analogWrite(10, 128); 
+
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
+    digitalWrite(33, HIGH);
+    delay(1000);
+    digitalWrite(33, LOW);
+    delay(1000);
+
+    analogWrite(10, 0);
+    delay(1000);
+    analogWrite(10, 255);
+    delay(1000);
+
+
 }
