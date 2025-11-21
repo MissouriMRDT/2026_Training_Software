e8c8f2694955c2a962cb7887ba19a4e1e7445454
aidengithub12 <147771123+aidengithub12@users.noreply.github.com>
2025-11-20 20:31:52 -0600

    servo work

---
+++ include/pinAssignments.h
@@ -0,0 +1,2 @@
+#define servoPin 2
+#define ledpin 8
--- src/TrainingSoftware.cpp
+++ src/TrainingSoftware.cpp
@@ -1,9 +1,20 @@
+#include "pinAssignments.h"
 #include <Arduino.h>
+#include <Servo.h>
 
+Servo servo;
 void setup() {
     // put your setup code here, to run once:
+    Serial.begin(115200);
+    servo.attach(servoPin);
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
+    // put your main code h`ere, to run repeatedly:
+    Serial.println("20");
+    servo.write(20);
+    delay(1000);
+    Serial.println("120");
+    servo.write(120);
+    delay(1000);
 }
